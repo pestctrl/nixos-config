5b2d626dc3d6acd91563a5875e2306907e90526c
Benson Chu <bensonchu457@gmail.com>
2023-12-07 19:29:19 -0600

    Duplication

--- common-configuration.nix
+++ common-configuration.nix
@@ -37,22 +37,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "dvorak";
-  };
-
-  # Configure console keymap
-  console.keyMap = "dvorak";
-
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
@@ -75,18 +59,6 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.benson = {
-    isNormalUser = true;
-    description = "Benson Chu";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      kate
-    #  thunderbird
-    ];
-  };
 
   fonts = {
     packages = with pkgs; [
