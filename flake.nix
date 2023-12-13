7b8f9a76b7a87911c9778f0b634e9f66ac757fca
Benson Chu <bensonchu457@gmail.com>
2023-12-12 19:41:50 -0600

    Add the overlay for now, but it doesn't do anything

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    emacs-overlay.url = "github:nix-community/emacs-overlay/da2f552d133497abd434006e0cae996c0a282394";
   };
 
   outputs = { self, nixpkgs, ... }: {
+    nixpkgs.overlays = [ (import self.inputs.emacs-overlay) ];
+
     nixosConfigurations.NixFrame = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./hosts/NixFrame/configuration.nix ];
