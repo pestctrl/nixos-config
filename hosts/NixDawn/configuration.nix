27cac979fd815911715e1a03e93a543e6b6b3c00
Benson Chu <bensonchu457@gmail.com>
2024-01-09 17:55:27 -0600

    Hm, steam apparently needs even more

--- hosts/NixDawn/configuration.nix
+++ hosts/NixDawn/configuration.nix
@@ -43,6 +43,11 @@ in
   programs = {
     evince.enable = true;
     thunar.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    };
   };
 
   services = {
