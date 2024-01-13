354b1588d26e0a8900ed810825250779efcf7b0e
Benson Chu <bensonchu457@gmail.com>
2024-01-13 17:20:24 -0600

    it works now

--- common/moms-house/printer.nix
+++ common/moms-house/printer.nix
@@ -1,21 +1,27 @@
 { config, pkgs, ... }:
 
 {
-  # Enable CUPS to print documents.
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      cups-brother-mfcl2750dw
-      brlaser
-      brgenml1lpr
-      brgenml1cupswrapper
-    ];
+  services = {
+    # Enable CUPS to print documents.
+    printing = {
+      enable = true;
+      drivers = with pkgs; [
+        cups-brother-mfcl2750dw
+        brlaser
+        brgenml1lpr
+        brgenml1cupswrapper
+      ];
+    };
+
+    # Enable auto-discovery of printers
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      openFirewall = true;
+    };
   };
 
-  # Enable auto-discovery of printers
-  services.avahi = {
-    enable = true;
-    nssmdns = true;
-    openFirewall = true;
+  networking.hosts = {
+    "192.168.1.78" = [ "BRWDCE994530FB4.local" "BRWDCE994530FB4" ];
   };
 }
