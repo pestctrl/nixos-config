12aa874f97b9183a50433cb6bfef864361064c10
Benson Chu <bensonchu457@gmail.com>
2024-01-28 15:01:43 -0600

    So that's why there were 2 nixpkgs!

--- flake.nix
+++ flake.nix
@@ -2,7 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    emacs-overlay.url = "github:nix-community/emacs-overlay/master";
+    emacs-overlay = {
+      url = "github:nix-community/emacs-overlay/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
