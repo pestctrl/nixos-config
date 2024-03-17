a3b501299a6bb826846fcc533d37b9c517e817ba
Benson Chu <bensonchu457@gmail.com>
2024-03-17 15:13:23 -0500

    local paths

--- common/moms-house/hosts.nix
+++ common/moms-house/hosts.nix
@@ -2,8 +2,8 @@
 
 {
   networking.hosts = {
-    "192.168.0.10" = [ "ProxFaith" ];
-    "192.168.0.102" = [ "TrueFaith" ];
-    "192.168.0.100" = [ "FedoraFaith" ];
+    "192.168.0.10" = [ "ProxFaith" "proxfaith.local" ];
+    "192.168.0.102" = [ "TrueFaith" "truefaith.local" ];
+    "192.168.0.100" = [ "FedoraFaith" "fedorafaith.local" ];
   };
 }
