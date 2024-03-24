271869ed6620da15db1adc758166bf2138ac1c8a
Benson Chu <bensonchu457@gmail.com>
2024-03-23 20:14:47 -0500

    This is now controlled by opnsense

--- common/moms-house/hosts.nix
+++ common/moms-house/hosts.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  networking.hosts = {
-    "192.168.0.10" = [ "ProxFaith" "proxfaith.local" ];
-    "192.168.0.102" = [ "TrueFaith" "truefaith.local" ];
-    "192.168.0.100" = [ "FedoraFaith" "fedorafaith.local" ];
-  };
+  # networking.hosts = {
+  #   "192.168.0.10" = [ "ProxFaith" "proxfaith.local" ];
+  #   "192.168.0.102" = [ "TrueFaith" "truefaith.local" ];
+  #   "192.168.0.100" = [ "FedoraFaith" "fedorafaith.local" ];
+  # };
 }
