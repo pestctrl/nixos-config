0dd9f54216714022d9f6bc7d696c5071981b2c77
Benson Chu <bensonchu457@gmail.com>
2024-05-06 18:18:00 -0500

    Hostname changed

--- hosts/NixGate/configuration.nix
+++ hosts/NixGate/configuration.nix
@@ -17,7 +17,7 @@
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "NixGate"; # Define your hostname.
+  networking.hostName = "NixSentinel"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
