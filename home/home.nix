5e4a56b2f5aa3c0bbba7d816d1e7c4d96ea48edd
Benson Chu <bensonchu457@gmail.com>
2024-07-13 17:35:46 -0500

    Ugh, my attempt at an impure solution.
    Perhaps nvfetcher is the direction I should be moving in.

--- home/home.nix
+++ home/home.nix
@@ -96,6 +96,14 @@
         mu4e
       ];
     };
+
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
   };
 
   services = {
--- home/modules/bash-config/default.nix
+++ home/modules/bash-config/default.nix
@@ -2,24 +2,34 @@
 let
   cfg = config.my.bash-config;
 
-  bash-drv = pkgs.stdenv.mkDerivation {
-    name = "bash-config";
-    src = inputs.bashcfg-input;
-    dontBuild = true;
-    installPhase = ''
-      mkdir -p $out
-      cp *.sh $out
-    '';
+  # bash-drv = pkgs.stdenv.mkDerivation {
+  #   name = "bash-config";
+  #   src = inputs.bashcfg-input;
+  #   dontBuild = true;
+  #   installPhase = ''
+  #     mkdir -p $out
+  #     cp *.sh $out
+  #   '';
+  # };
+
+  bash-drv = builtins.fetchGit {
+    url = "https://github.com/pestctrl/bash-config";
+    ref = "master";
   };
 in
 {
-  options.my.bash-config.enable = lib.mkEnableOption "Enable bash configuration file";
+  options = {
+    my.bash-config.enable = lib.mkEnableOption "Enable bash configuration file";
+  };
 
   config = lib.mkIf cfg.enable {
     # Why can't I do the following?
     # home.file."${config.xdg.configHome}/bash-config/emacs.sh"
-    xdg.configFile."bash-config/emacs.sh".source = "${bash-drv}/emacs.sh";
+    # xdg.configFile."bash-config/emacs.sh".source = "${bash-drv}/emacs.sh";
 
-    home.file.".bashrc".source = "${bash-drv}/bashrc.sh";
+    # home.file.".bashrc".source = "${bash-drv}/bashrc.sh";
+    programs.bash.bashrcExtra = ''
+      source ${bash-drv}/bashrc.sh
+    '';
   };
 }
