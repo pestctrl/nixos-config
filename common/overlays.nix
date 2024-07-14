4d64ef3eea9af3823b42d58125eb621b0f60d34a
Benson Chu <bensonchu457@gmail.com>
2024-07-14 18:12:36 -0500

    Comments

--- common/overlays.nix
+++ common/overlays.nix
@@ -3,7 +3,11 @@ inputs:
   (final: prev: {
     mps-debug = prev.mps.overrideAttrs (old: {
       pname = old.pname + "-debug";
-      env.NIX_CFLAGS_COMPILE = old.env.NIX_CFLAGS_COMPILE + " -g -DCONFIG_VAR_COOL";
+      # Need to do this to patch dwarf information. For some reason,
+      # stdenv copies the source into /build/source, and then builds
+      # from there, leaving all the dwarf information hanging.
+      env.NIX_CFLAGS_COMPILE =
+        old.env.NIX_CFLAGS_COMPILE + " " + "-fdebug-prefix-map=/build/source=${old.src}";
       dontStrip = true;
     });
   })
