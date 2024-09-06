e405ca60c09eb572d78c2cddb66b15adabb53208
Benson Chu <bensonchu457@gmail.com>
2024-09-06 13:32:04 -0500

    mps no longer has a NIX_CFLAGS_COMPILE

--- common/overlays.nix
+++ common/overlays.nix
@@ -6,8 +6,7 @@ inputs:
       # Need to do this to patch dwarf information. For some reason,
       # stdenv copies the source into /build/source, and then builds
       # from there, leaving all the dwarf information hanging.
-      env.NIX_CFLAGS_COMPILE =
-        old.env.NIX_CFLAGS_COMPILE + " " + "-fdebug-prefix-map=/build/source=${old.src}";
+      env.NIX_CFLAGS_COMPILE = "-fdebug-prefix-map=/build/source=${old.src}";
       dontStrip = true;
     });
   })
