e22db4cd2a9be600f2ea0881cbd5e3331285cfcd
Benson Chu <bensonchu457@gmail.com>
2025-06-19 09:02:14 -0500

    Draft 2

--- common/dotsies.nix
+++ common/dotsies.nix
@@ -3,64 +3,20 @@
 stdenv.mkDerivation rec {
   pname = "dotsies-fonts";
   version = "1";
-  dotsies = fetchurl {
-    url = "http://dotsies.org/Dotsies.ttf";
-  };
-  dotsies-training = fetchurl {
-    url = "http://dotsies.org/Dotsies%20Training%20Wheels.ttf";
-  };
-  dotsies-wide = fetchurl {
-    url = "http://dotsies.org/Dotsies%20Wide.ttf";
-  };
-  dotsies-context = fetchurl {
-    url = "http://dotsies.org/Dotsies%20Context.ttf";
-  };
-  dotsies-stripped = fetchurl {
-    url = "http://dotsies.org/Dotsies%20Striped.ttf";
-  };
-  dotsies-roman = fetchurl {
-    url = "http://dotsies.org/Dotsies%20Roman.ttf";
-  };
-  dotsies-conf = fetchurl {
-    url = "21-dotsies.conf";
-  };
-
-  pro = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
-    sha256 = "sha256-g/SzgU3vOzm8uRuA6AN/N8Tnrl2Vpya58hx99dGfecI=";
-  };
-
-  compact = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-    sha256 = "sha256-SIht9sqmeijEeU4uLwm+tlZtFlTnD/G5GH8haUL6dlU=";
-  };
-
-  mono = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-    sha256 = "sha256-jnhTTmSy5J8MJotbsI8g5hxotgjvyDbccymjABwajYw=";
-  };
-
-  ny = fetchurl {
-    url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
-    sha256 = "sha256-Rr0UpJa7kemczCqNn6b8HNtW6PiWO/Ez1LUh/WNk8S8=";
-  };
-
-  nativeBuildInputs = [ p7zip ];
 
   sourceRoot = ".";
 
   dontUnpack = true;
 
   installPhase = ''
-    mkdir -p $out/usr/share/fonts/TTF
+    mkdir -p $out/share/fonts/truetype
 
-    mv ${dotsies} $out/usr/share/fonts/TTF
-    mv ${dotsies} $out/usr/share/fonts/TTF
+    cp ${../res/font/Dotsies.ttf} $out/share/fonts/truetype
   '';
 
   meta = {
-    description = "Apple San Francisco, New York fonts";
-    homepage = "https://developer.apple.com/fonts/";
+    description = "Dotsies fonts";
+    homepage = "https://dotsies.org/";
     license = lib.licenses.unfree;
   };
 }
