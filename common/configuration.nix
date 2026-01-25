b365d28181b20d0d4eb899cb533881bf30bdfd4a
Benson Chu <dev@mail.pestctrl.io>
2026-01-25 08:53:51 -0600

    Make dolphin usable

--- common/configuration.nix
+++ common/configuration.nix
@@ -25,6 +25,20 @@ in
         config.allowUnfree = true;
       };
     })
+    # https://discourse.nixos.org/t/dolphin-does-not-have-mime-associations/48985/14
+    # https://github.com/rumboon/dolphin-overlay/blob/main/default.nix
+    (final: prev: {
+      kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+        dolphin = kprev.dolphin.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
+          postInstall = (oldAttrs.postInstall or "") + ''
+        wrapProgram $out/bin/dolphin \
+            --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+            --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+      '';
+        });
+      });
+    })
   ] ++ import ./overlays.nix inputs ++ [
     inputs.emacs-overlay.overlays.default
   ];
