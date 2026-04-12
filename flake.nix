ecf15a0b13e4226b647a81bfdb5f6f92192f4273
Benson Chu <dev@mail.pestctrl.io>
2026-04-12 16:31:46 -0500

    More precise

--- flake.nix
+++ flake.nix
@@ -63,7 +63,7 @@
             ];
           };
 
-          NixGate = pkgs.lib.nixosSystem {
+          NixGate = nixpkgs.lib.nixosSystem {
             inherit system;
             specialArgs = { inherit inputs; };
             modules = [
@@ -72,7 +72,7 @@
             ];
           };
 
-          NixSentinel = pkgs.lib.nixosSystem {
+          NixSentinel = nixpkgs.lib.nixosSystem {
             inherit system;
             specialArgs = { inherit inputs; };
             modules = [
