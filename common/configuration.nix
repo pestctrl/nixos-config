042030359b598ea2fb072aec81874b6d0f798618
Benson Chu <dev@mail.pestctrl.io>
2026-04-12 16:38:10 -0500

    These overlays are in the flake

--- common/configuration.nix
+++ common/configuration.nix
@@ -12,6 +12,35 @@ in
     ./flake-location.nix
   ];
 
+  nixpkgs.overlays = [
+    (final: prev: {
+      unstable = import inputs.unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    })
+    (final: prev: {
+      update = import inputs.update {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    })
+    # https://discourse.nixos.org/t/dolphin-does-not-have-mime-associations/48985/14
+    # https://github.com/rumboon/dolphin-overlay/blob/main/default.nix
+    (final: prev: {
+      kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+        dolphin = kprev.dolphin.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
+          postInstall = (oldAttrs.postInstall or "") + ''
+        wrapProgram $out/bin/dolphin \
+            --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+            --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+      '';
+        });
+      });
+    })
+  ];
+
   nix = {
     settings.experimental-features = "nix-command flakes";
 
